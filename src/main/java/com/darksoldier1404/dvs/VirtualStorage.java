415eea6f6b8e64f6127c4cde043edf459ef0317e
darksoldier1404 <darksoldier1404@gmail.com>
2021-11-18 18:54:49 +0900

    added check DP-UniversalCore plugin is installed

--- src/main/java/com/darksoldier1404/dvs/VirtualStorage.java
+++ src/main/java/com/darksoldier1404/dvs/VirtualStorage.java
@@ -1,5 +1,6 @@
 package com.darksoldier1404.dvs;
 
+import com.darksoldier1404.duc.UniversalCore;
 import com.darksoldier1404.dvs.commands.DVSCommand;
 import com.darksoldier1404.dvs.events.DVSEvent;
 import com.darksoldier1404.dvs.functions.DVSFunction;
@@ -9,6 +10,7 @@ import com.earth2me.essentials.Essentials;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.HumanEntity;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
@@ -16,6 +18,7 @@ import java.util.Map;
 import java.util.UUID;
 
 public class VirtualStorage extends JavaPlugin {
+    private UniversalCore core;
     private static VirtualStorage plugin;
     public String prefix;
     public YamlConfiguration config;
@@ -29,6 +32,13 @@ public class VirtualStorage extends JavaPlugin {
 
     public void onEnable() {
         plugin = this;
+        Plugin pl = getServer().getPluginManager().getPlugin("UniversalCore");
+        if(pl == null) {
+            getLogger().warning("DP-UniversalCore 플러그인이 설치되어있지 않습니다.");
+            plugin.setEnabled(false);
+            return;
+        }
+        core = (UniversalCore) pl;
         ess = (Essentials) Bukkit.getPluginManager().getPlugin("Essentials");
         getLogger().info("VirtualStorage has been enabled!");
         ConfigUtils.loadDefaultConfig();
